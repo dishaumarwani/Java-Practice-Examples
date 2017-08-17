9e7165874050c18c94beaa8b2f75e6f03bedb841
dishaumarwani <dhu200@nyu.edu>
2017-08-17 00:10:48 -0400

    initial commit

--- Collections/TestCollection1.java
+++ Collections/TestCollection1.java
@@ -1,11 +1,14 @@
 import java.util.*;
 class TestCollection1{
 	public static void main(String args[]){
+		/* Syntax to create ArrayList in Java after Java 7 */
 		List<String> ar = new ArrayList<>();
+		/* Adding elements in ArrayList */
 		ar.add("John");
 		ar.add("Marta");
 		ar.add("Simpson");
 		ar.add("Kohl");
+		/*iterate over a list by using Iterator Class*/
 		Iterator itr = ar.iterator();
 		while (itr.hasNext()){
 			System.out.println(itr.next());
